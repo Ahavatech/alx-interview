51db07d17654a4b99b82ec99cde9d06f43053ee8
[Ibukunoluwa Adekunle] <[ibkadekunle2004@gmail.com]>
2024-02-20 17:13:40 +0100

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,24 @@
-#!/usr/bin/node
+const axios = require('axios');
 
-const request = require('request');
-
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
+async function getCharacters(movieId) {
+    try {
+        const response = await axios.get(`https://swapi.dev/api/films/${movieId}/`);
+        const filmData = response.data;
+        const characters = filmData.characters;
+        for (const characterUrl of characters) {
+            const characterResponse = await axios.get(characterUrl);
+            console.log(characterResponse.data.name);
+        }
+    } catch (error) {
+        console.error("Error:", error.message);
     }
-  });
-};
+}
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
-  }
-);
+if (process.argv.length !== 3) {
+    console.log("Usage: node script.js <Movie ID>");
+    process.exit(1);
+}
+
+const movieId = process.argv[2];
+getCharacters(movieId);
+
